0464752a464af3d0b39017dbb632f57028b86bb0
AltynaiDautova <altynajdautova5@gmail.com>
2023-12-19 12:53:16 +0600

    Altynais code

--- home_page/urls.py
+++ home_page/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import string_post_view, restaurants_detail_view, SearchResultsView, HomePageView
 
 
-app_name='home_page'
+app_name = 'home_page'
 urlpatterns = [
     path('home_page/', string_post_view, name='home_page'),
     path('restaurant_detail/<int:id>/', restaurants_detail_view),
--- persons/models.py
+++ persons/models.py
@@ -2,15 +2,23 @@ from django.db import models
 
 
 class Person(models.Model):
-    GENDER = (
-        ('M', 'M'),
-        ('F', "F"),
+    GENRE = (
+        ('Russian', 'Russian'),
+        ('Italian', 'Italian'),
+        ('American', 'American'),
+        ('Japanese', 'Japanese'),
+        ('Chinese', 'Chinese'),
+
     )
-    name = models.CharField(max_length=100, verbose_name='Enter your name')
-    age = models.PositiveIntegerField(verbose_name='Enter your age')
-    hobby = models.TextField(verbose_name='Your Hobby')
-    gender = models.CharField(max_length=100, choices=GENDER)
+    name = models.CharField(max_length=100, verbose_name="Enter your restaurant name", null=True)
+    description = models.TextField(null=True)
+    average_cost = models.PositiveIntegerField(null=True)
+    genre = models.CharField(max_length=100, choices=GENRE, null=True)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    address = models.TextField(null=True)
+    contact_number = models.CharField(max_length=15,null=True)
     phone_number = models.CharField(max_length=14, default='+996', verbose_name='Your Number')
 
     def __str__(self):
         return self.name
+
